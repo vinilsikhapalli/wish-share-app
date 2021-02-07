21f2204f3a0e98c141603c2fdd873def6e3ab1d4
vinil2426@gmail.com <vinil2426@gmail.com>
2021-02-07 13:45:36 +0530

    here is our new project [wishes-share-app]

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+        <h1>from Header developed by vinil </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .header {
+  padding: 20px 30px 20px 30px;
+  /* border: 2px solid #f76707; */
+  background-color: #8ebfe7;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(2, 100px);
+}
+
+</style>
