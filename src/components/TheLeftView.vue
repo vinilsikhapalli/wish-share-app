58b7ab5d3703ff3e38b1664d3fb7812e2d682962
vinil2426@gmail.com <vinil2426@gmail.com>
2021-02-08 12:46:58 +0530

    added stylings to left component

--- src/components/TheLeftView.vue
+++ src/components/TheLeftView.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="leftComoponent col-md-6 col-sm-6">
-        <h1>here from left side</h1>
+        <div class="heart-shape text-center col-6 ">
+          <!-- <h2>hello world from here</h2> -->
+        </div>
     </div>
 </template>
 <script>
@@ -10,14 +12,46 @@ export default {
 </script>
 <style scoped>
 .leftComoponent {
-  padding: 20px 10px 20px 20px;
-  /* border: 2px solid #f76707; */
+  padding: 50px 10px 50px 20px;
+  border: 2px solid #f76707;
   /* border-radius: 5px; */
   background-color: #c2c5eb;
-  -ms-transform: skewY(0deg); /* IE 9 */
-  transform: skewY(-40deg);
   /* display: grid; */
-  grid-template-columns: 1fr 2fr;
-  grid-template-rows: repeat(3, 100px);
+  /* grid-template-columns: 1fr 2fr; */
+  /* grid-template-rows: repeat(3, 100px); */
+}
+.heart-shape{
+  position: relative;
+  width: 145px;
+  height: 145px;
+  -webkit-transform: rotate(45deg);
+  -moz-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  -o-transform: rotate(45deg);
+  transform: rotate(45deg);
+  background-color: rgb(250, 66, 152);
+}
+.heart-shape:before,
+.heart-shape:after{
+  position: absolute;
+  width: 145px;
+  height: 145px;
+  content: '';
+  -webkit-border-radius: 50%;
+  -moz-border-radius: 50%;
+  -o-border-radius: 50%;
+  border-radius: 50%;
+  background-color: rgb(250, 66, 152);
+}
+.heart-shape:before{
+  bottom: 0px;
+  left: -50px;
+}
+.heart-shape:after{
+  top: -50px;
+  right: 0px;
 }
 </style>
+// out line colur 
+// content senter
+// content on heart
